9280de0a076a40beaae952e17bca2a7e701b5ae6
Tharindu1527 <tharindubandara15270@gmail.com>
2025-03-31 13:34:51 +0530

    Adding pipelines

--- Stock-predictions-backend/ml/tests.py
+++ Stock-predictions-backend/ml/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTestCase(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
