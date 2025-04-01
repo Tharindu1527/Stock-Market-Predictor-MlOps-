b485b54260819af85ea31386b3211bc190228cd7
Tharindu1527 <tharindubandara15270@gmail.com>
2025-04-01 22:47:00 +0530

    add

--- Stock-predictions-backend/ml/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-class SimpleTestCase(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
